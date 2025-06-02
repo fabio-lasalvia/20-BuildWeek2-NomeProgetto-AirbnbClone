ec01a52e77487f7bcbd077865b99a51c97e93b62
Iolanda02 <iolanda.anile@gmail.com>
2025-06-03 00:38:35 +0200

    costruzione dinamica pagina risultati + collegamento pagina dettaglio

--- airbnb.js
+++ airbnb.js
@@ -1,13 +1,252 @@
+const ALLOGGI = [
+  {
+    id: 1,
+    images: [
+      {src: 'https://a0.muscache.com/im/pictures/miso/Hosting-1417807494670456186/original/c791bdb0-287e-4ce6-8c28-848028b34bc0.jpeg',
+        alt: 'Immagine 1'
+      },
+      {src: 'https://a0.muscache.com/im/pictures/miso/Hosting-1417807494670456186/original/7e4fb49d-b19d-4d23-b9c1-6f9641dbb94d.jpeg',
+        alt: 'Immagine 2'
+      },
+      {src: 'https://a0.muscache.com/im/pictures/miso/Hosting-1417807494670456186/original/125594a6-81e7-4e99-9e2a-07e63b8686c1.jpeg',
+        alt: 'Immagine 3'
+      },
+      {src: 'https://a0.muscache.com/im/pictures/miso/Hosting-1417807494670456186/original/2cc1348d-0e7d-4d87-a209-33813a254353.jpeg',
+        alt: 'Immagine 4'
+      },
+      {src: 'https://a0.muscache.com/im/pictures/miso/Hosting-1417807494670456186/original/f5531cdd-b325-42da-b9b4-644abd2f9754.jpeg',
+        alt: 'Immagine 5'
+      },
+      {src: 'https://a0.muscache.com/im/pictures/miso/Hosting-1417807494670456186/original/12e5d53b-80bd-45f4-9f8b-318be28cb713.jpeg',
+        alt: 'Immagine 6'
+      },
+      {src: 'https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTQxNzgwNzQ5NDY3MDQ1NjE4Ng==/original/15c1bc7c-bb69-4eb0-9607-d9f556cfe510.png',
+        alt: 'Immagine 7'
+      }
+    ],
+    leftTextTitle: 'Appartamento &middot; Roma',
+    rightTextTitle: 'Nuovo',
+    description: 'Pigneto Nuova collezione 2',
+    places: '1 letto',
+    supplier: 'Host privato',
+    cost: {
+      strong: '84 &euro;',
+      normal: 'notte'
+    },
+    totalCost: '504 &euro; in totale'
+  },
+  {
+    id: 2,
+    images: [
+      {src: 'https://a0.muscache.com/im/pictures/hosting/Hosting-1119629382996529815/original/144c54ea-a7c5-44ff-a3e0-7efa75254653.jpeg',
+        alt: 'Immagine 1'
+      },
+      {src: 'https://a0.muscache.com/im/pictures/hosting/Hosting-1119629382996529815/original/4ff4d85a-d29e-4a1f-aa7b-f54f73859168.jpeg',
+        alt: 'Immagine 2'
+      },
+      {src: 'https://a0.muscache.com/im/pictures/hosting/Hosting-1119629382996529815/original/3e69e186-60a6-47fd-9398-62c2b0b6db37.jpeg',
+        alt: 'Immagine 3'
+      },
+      {src: 'https://a0.muscache.com/im/pictures/hosting/Hosting-1119629382996529815/original/ffa88c99-ab06-4926-970f-d7852951f1fe.jpeg',
+        alt: 'Immagine 4'
+      },
+      {src: 'https://a0.muscache.com/im/pictures/hosting/Hosting-1119629382996529815/original/a9c344b0-eea0-4476-8fb8-eb79b6d8fc51.jpeg',
+        alt: 'Immagine 5'
+      },
+      {
+        src: 'https://a0.muscache.com/im/pictures/hosting/Hosting-1119629382996529815/original/3f4c46f7-76ce-4952-bd55-e9fefbbc503a.jpeg',
+        alt: 'Immagine 6'
+      },
+      {
+        src: 'https://a0.muscache.com/im/pictures/hosting/Hosting-1119629382996529815/original/10524c60-f732-4a29-86d4-1972c1e49cd3.jpeg',
+        alt: 'Immagine 7'
+      }
+    ],
+    leftTextTitle: 'Appartamento &middot; Roma',
+    rightTextTitle: '5,0 (14)',
+    description: 'Alloggio Don Bosco vicino alla metro A',
+    places: '4 letti',
+    supplier: 'Host privato',
+    cost: {
+      strikethrough: '122 &euro;',
+      strong: '79 &euro;',
+      normal: 'notte'
+    },
+    totalCost: '474 &euro; in totale'
+  },
+  {
+    id: 3,
+    images: [
+      {src: 'https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTE2NTY4OTk5NTczOTE2MTI4OA%3D%3D/original/5faa39f5-6b7d-462a-9524-5212c0efe6fe.jpeg',
+        alt: 'Immagine 1'
+      },
+      {src: 'https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTE2NTY4OTk5NTczOTE2MTI4OA%3D%3D/original/cde8aa7b-3b17-4fb6-9aaf-76b8d5427cf2.jpeg',
+        alt: 'Immagine 2'
+      },
+      {src: 'https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTE2NTY4OTk5NTczOTE2MTI4OA%3D%3D/original/6528a7d6-7b34-4cce-86ea-fef0b2d06b5b.jpeg',
+        alt: 'Immagine 3'
+      },
+      {src: 'https://a0.muscache.com/im/pictures/miso/Hosting-1165689995739161288/original/ece16047-723e-493e-b034-1baae3cfce07.jpeg',
+        alt: 'Immagine 4'
+      },
+      {src: 'https://a0.muscache.com/im/pictures/miso/Hosting-1165689995739161288/original/8609a523-404c-45ce-b493-55e84c5b711c.jpeg',
+        alt: 'Immagine 5'
+      },
+      {
+        src: 'https://a0.muscache.com/im/pictures/miso/Hosting-1165689995739161288/original/05e43123-8ceb-4479-8add-b86fb0af4b92.jpeg',
+        alt: 'Immagine 6'
+      },
+      {
+        src: 'https://a0.muscache.com/im/pictures/miso/Hosting-1165689995739161288/original/b4b0d98d-cdab-40e6-83cf-748389a84692.jpeg',
+        alt: 'Immagine 7'
+      }
+    ],
+    leftTextTitle: 'Appartamento &middot; Pomezia',
+    rightTextTitle: '4,94 (32)',
+    description: 'LucyElegance a 800MT dalla Stazione di Pomezia',
+    places: '2 letti',
+    supplier: 'Host professionista',
+    cost: {
+      strikethrough: '72 &euro;',
+      strong: '62 &euro;',
+      normal: 'notte'
+    },
+    totalCost: '368 &euro; in totale'
+  },
+  {
+    id: 4,
+    images: [
+      {src: 'https://a0.muscache.com/im/pictures/miso/Hosting-1170962571409107811/original/d37e0e41-c6fb-4192-bae2-4c2fca9dead9.jpeg',
+        alt: 'Immagine 1'
+      },
+      {src: 'https://a0.muscache.com/im/pictures/miso/Hosting-1170962571409107811/original/da76e358-d882-4cc9-9353-74d4f06cd8af.jpeg',
+        alt: 'Immagine 2'
+      },
+      {src: 'https://a0.muscache.com/im/pictures/miso/Hosting-1170962571409107811/original/57803497-7cfc-4db1-bbad-02e7d46c66e2.jpeg',
+        alt: 'Immagine 3'
+      },
+      {src: 'https://a0.muscache.com/im/pictures/miso/Hosting-1170962571409107811/original/d76847fa-9e04-444f-979e-80e3056f3710.jpeg',
+        alt: 'Immagine 4'
+      },
+      {src: 'https://a0.muscache.com/im/pictures/miso/Hosting-1170962571409107811/original/4e57a998-5d26-4472-b788-9ff1241278e8.jpeg',
+        alt: 'Immagine 5'
+      },
+      {src: 'https://a0.muscache.com/im/pictures/miso/Hosting-1170962571409107811/original/cd6c5707-b652-45ee-bfa0-501597fa18b9.jpeg',
+        alt: 'Immagine 6'
+      },
+      {src: 'https://a0.muscache.com/im/pictures/miso/Hosting-1170962571409107811/original/11e73103-4c7f-4f87-bcae-ec8c2ba0313d.jpeg',
+        alt: 'Immagine 7'
+      }
+    ],
+    leftTextTitle: 'Appartamento &middot; Roma',
+    rightTextTitle: '4,9 (40)',
+    description: 'Casa con giardino privato Villa Ada | Terrestre',
+    places: '2 letti',
+    supplier: 'Host professionista',
+    cost: {
+      strikethrough: '180 &euro;',
+      strong: '122 &euro;',
+      normal: 'notte'
+    },
+    totalCost: '366 &euro; in totale'
+  },
+  {
+    id: 5,
+    images: [
+      {src: 'https://a0.muscache.com/im/pictures/miso/Hosting-42151648/original/a2869fe2-80e9-49c6-bc29-abd1a33f7a1f.jpeg',
+        alt: 'Immagine 1'
+      },
+      {src: 'https://a0.muscache.com/im/pictures/miso/Hosting-42151648/original/85a64bcc-9c05-46dc-8f69-6a3f0b751fcc.jpeg',
+        alt: 'Immagine 2'
+      },
+      {src: 'https://a0.muscache.com/im/pictures/93e73482-b91e-4fb2-b311-03025d96d771.jpg',
+        alt: 'Immagine 3'
+      },
+      {src: 'https://a0.muscache.com/im/pictures/miso/Hosting-42151648/original/f8e532ca-a087-4955-9e71-46be2bb31652.jpeg',
+        alt: 'Immagine 4'
+      },
+      {src: 'https://a0.muscache.com/im/pictures/miso/Hosting-42151648/original/d0749db0-5c8c-4d69-a66b-6bda2dd91968.jpeg',
+        alt: 'Immagine 5'
+      },
+      {src: 'https://a0.muscache.com/im/pictures/miso/Hosting-42151648/original/16425e54-9dc1-4389-a129-a47c87dfc2b1.jpeg',
+        alt: 'Immagine 6'
+      },
+      {src: 'https://a0.muscache.com/im/pictures/miso/Hosting-42151648/original/da15a5e5-5b31-4436-bdff-a4996c0f24a9.jpeg',
+        alt: 'Immagine 7'
+      }
+    ],
+    leftTextTitle: 'Casa &middot; Roma',
+    rightTextTitle: '4,73 (48)',
+    description: 'Apt 3 - Tenuta Capizucchi - Relax + Giardino',
+    places: '5 letti',
+    supplier: 'Host professionista',
+    cost: {
+      strikethrough: '179 &euro;',
+      strong: '95 &euro;',
+      normal: 'notte'
+    },
+    totalCost: '569 &euro; in totale'
+  },
+  {
+    id: 6,
+    images: [
+      {src: 'https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTMwMDY2NDg5NzI3NTM2OTYxNA%3D%3D/original/49cb773c-51f1-47f2-8543-16b5213c28d1.jpeg',
+        alt: 'Immagine 1'
+      },
+      {src: 'https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTMwMDY2NDg5NzI3NTM2OTYxNA%3D%3D/original/3a6972da-034c-459c-8637-07f0b364e8b2.jpeg',
+        alt: 'Immagine 2'
+      },
+      {src: 'https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTMwMDY2NDg5NzI3NTM2OTYxNA%3D%3D/original/0928eb38-7ea2-4a17-8949-c9615adb4a13.jpeg',
+        alt: 'Immagine 3'
+      },
+      {src: 'https://a0.muscache.com/im/pictures/miso/Hosting-1300664897275369614/original/472c83fd-ddc1-44a4-aa78-476b744fe987.jpeg',
+        alt: 'Immagine 4'
+      },
+      {src: 'https://a0.muscache.com/im/pictures/miso/Hosting-1300664897275369614/original/24425f44-aa54-4e89-a1dd-f6760a99a17f.jpeg',
+        alt: 'Immagine 6'
+      },
+      {src: 'https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTMwMDY2NDg5NzI3NTM2OTYxNA%3D%3D/original/f7bee546-6609-4000-930a-259534c1ac79.jpeg',
+        alt: 'Immagine 7'
+      },
+      {src: 'https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTMwMDY2NDg5NzI3NTM2OTYxNA%3D%3D/original/6afa5b84-91d4-4cda-8ff2-2e22939d95b7.jpeg',
+        alt: 'Immagine 7'
+      }
+    ],
+    leftTextTitle: 'Appartamento &middot; Fiumicino',
+    rightTextTitle: '5,0 (7)',
+    description: '[Liberty House] aeroporto 5 minuti',
+    places: '1 letto',
+    supplier: 'Host privato',
+    cost: {
+      strikethrough: '102 &euro;',
+      strong: '79 &euro;',
+      normal: 'notte'
+    },
+    totalCost: '315 &euro; in totale'
+  }
+]
+
+
+
 // Gestione caricamento pagine
 const PATH_PAGINA_AIRBNB = "airbnbMioCSS.html";
+const PATH_PAGINA_HOME= "home.html";
+const PATH_PAGINA_RISULTATI = "risultati.html";
+const PATH_PAGINA_DETTAGLIO = "dettaglio.html";
 
-function caricaAirbnbHome() {
-  console.log("Funzione caricaPaginaAirbnb eseguita!");
-  caricaPagina(PATH_PAGINA_AIRBNB);
+function caricaPaginaHome() {
+  caricaPagina(PATH_PAGINA_HOME);
+}
+
+function caricaPaginaRisultati() {
+  caricaPagina(PATH_PAGINA_RISULTATI);
+}
+
+function caricaPaginaDettaglio() {
+  caricaPagina(PATH_PAGINA_DETTAGLIO);
 }
 
 function caricaPagina(pagina) {
-  // console.log("caricaPagina ", pagina)
   window.location.replace(pagina);
 }
 
@@ -26,5 +265,108 @@ function decrementaOspite(type) {
   if (ospiti[type] > 0) {
     ospiti[type]--;
     document.getElementById(`${type}Count`).textContent = ospiti[type];
+  }
+}
+
+function loadResultsPage() {
+  document.getElementById('totalResults').innerHTML = `${ALLOGGI.length} ${ALLOGGI.length > 1? 'alloggi': 'alloggio'}`
+
+  const placeholderCard = document.getElementById('placeholderCard');
+  const containerCardCarousel = document.getElementById('containerCardCarousel');
+  
+  ALLOGGI.forEach((alloggio) => {
+    let card = placeholderCard.cloneNode(true);
+    card.id = 'cardCarousel' + alloggio.id;
+    card.querySelector('.card.card-carousel').id = 'card-' + alloggio.id; 
+    const idCarouselIndicators = 'carousel' + alloggio.id;
+    card.querySelector('.carousel.slide').id = idCarouselIndicators; 
+
+    const carouselIndicators = card.querySelector(`.carousel-indicators`);
+    const carouselInner = card.querySelector('.carousel-inner');
+    
+    for(let i=0; i<alloggio.images.length; i++) {
+      const button = document.createElement('button');
+      button.setAttribute('data-bs-target', '#' + idCarouselIndicators);
+      button.setAttribute('data-bs-slide-to', i);
+      button.setAttribute('aria-label', 'Slide ' + (i+1));
+
+      const carouselItem = document.createElement('div');
+      carouselItem.className = 'carousel-item';
+      const imageCarouselItem = document.createElement('img');
+      imageCarouselItem.src = alloggio.images[i].src;
+      imageCarouselItem.alt = alloggio.images[i].alt;
+      imageCarouselItem.className = 'd-block w-100';
+      carouselItem.appendChild(imageCarouselItem);
+      if(i == 0) {
+        button.setAttribute('aria-current', 'true');
+        button.classList.add('active');
+        carouselItem.classList.add('active');
+      }
+      carouselIndicators.appendChild(button)
+      carouselInner.appendChild(carouselItem);
+
+    }
+
+    const carouselControlPrev = card.querySelector('button.carousel-control-prev');
+    carouselControlPrev.setAttribute('data-bs-target', '#' + idCarouselIndicators);
+    const carouselControlNext = card.querySelector('button.carousel-control-next');
+    carouselControlNext.setAttribute('data-bs-target', '#' + idCarouselIndicators);
+
+    card.querySelector('.card-body .left-text-card-title').innerHTML = alloggio.leftTextTitle;
+    card.querySelector('.card-body .right-text-card-title').innerHTML = '&nbsp;' + alloggio.rightTextTitle;
+    card.querySelector('.card-body .description').innerHTML = alloggio.description;
+    card.querySelector('.card-body .places').innerHTML = alloggio.places;
+    card.querySelector('.card-body .supplier').innerHTML = alloggio.supplier;
+    if(alloggio.cost.strikethrough)
+      card.querySelector('.card-body .cost .strikethrough').innerHTML = alloggio.cost.strikethrough + '&nbsp;';
+    card.querySelector('.card-body .cost .strong').innerHTML = alloggio.cost.strong;
+    card.querySelector('.card-body .cost .normal').innerHTML = '&nbsp;' + alloggio.cost.normal;
+    card.querySelector('.card-body .cost .total-cost').innerHTML = alloggio.totalCost;
+
+    containerCardCarousel.appendChild(card);
+  })
+}
+
+function addCardListener() {
+  const containerCardCarousel = document.getElementById('containerCardCarousel');
+  for(const card of containerCardCarousel.children) {
+    if(card.id != "placeholderCard") {
+      card.addEventListener('click', ($event) => {
+        if(!$event.target.className.includes("carousel-control-prev") && !$event.target.className.includes("carousel-control-next") && !$event.target.className.includes("total-cost") && !$event.target.className.includes("active")) {
+          const cardId = $event.target.closest('.card.card-carousel').id.split('-')[1];
+          const cardData = ALLOGGI.find(obj => obj.id == cardId);
+          localStorage.setItem('selectedProduct', JSON.stringify(cardData));
+          caricaPaginaDettaglio()
+        }
+      })
+    }
+  }
+}
+
+
+function loadDetailPage() {
+  const storedProductJSON = localStorage.getItem('selectedProduct');
+  if(storedProductJSON) {
+    const productData = JSON.parse(storedProductJSON);
+    document.getElementById('title').innerHTML = productData.leftTextTitle;
+    document.getElementById('rating').innerHTML = productData.rightTextTitle + '&nbsp;ratings';
+
+    let mainImage = document.createElement('img');
+    mainImage.src = productData.images[0].src;
+    mainImage.alt = productData.images[0].alt;
+    mainImage.className = 'img-fluid rounded';
+    document.getElementById('main-image').appendChild(mainImage);
+
+    const containerImages = document.getElementById('container-images');
+    for(let i = 1; i < 5; i++) {
+      let container = document.createElement('div');
+      container.className = 'col-6';
+      let image = document.createElement('img');
+      image.src = productData.images[i].src;
+      image.alt = productData.images[i].alt;
+      image.className = 'img-fluid rounded';
+      container.appendChild(image);
+      containerImages.appendChild(container);
+    }
   }
 }
